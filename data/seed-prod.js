3e47f5c534c6c71b963c1a743552c048f4442bb1
LiamCurran <LiamCurran95>
2022-03-23 10:35:47 +0000

    adding-tests

--- data/seed-prod.js
+++ data/seed-prod.js
@@ -1,7 +1,6 @@
 const polygonAPI = require("./seed_prod_data/polygon");
-const polygonAPI2 = require("./seed_prod_data/polygon2");
-const ESGdata = require("./seed_prod_data/esg");
-const Userdata = require("./seed_prod_data/userdata");
+const esgData = require("./seed_prod_data/esg");
+const userData = require("./seed_prod_data/userdata");
 
 const ESG = require("../schema/esgSchema");
 const Polygon = require("../schema/polygonSchema");
@@ -9,32 +8,31 @@ const Users = require("../schema/usersSchema");
 
 const mongoose = require("mongoose");
 
-const uri = "INSERT MONGO URI HERE";
+const uri = "INSERT URI";
 
 const seedDB = async () => {
-  try {
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+	try {
+		await mongoose.connect(uri, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
 
-    await Polygon.deleteMany({});
-    await ESG.deleteMany({});
-    await Users.deleteMany({});
+		await Polygon.deleteMany({});
+		await ESG.deleteMany({});
+		await Users.deleteMany({});
 
-    await Polygon.insertMany(polygonAPI);
-    await Polygon.insertMany(polygonAPI2);
-    console.log(`stock entries added`);
+		await Polygon.insertMany(polygonAPI);
+		console.log(`stock entries added`);
 
-    await ESG.insertMany(ESGdata);
-    console.log(`esg entries added`);
+		await ESG.insertMany(esgData);
+		console.log(`esg entries added`);
 
-    await Users.insertMany(Userdata);
-    console.log(`user entries added`);
-  } catch (error) {
-    console.log(error);
-  } finally {
-    await mongoose.connection.close();
-  }
+		await Users.insertMany(userData);
+		console.log(`user entries added`);
+	} catch (error) {
+		console.log(error);
+	} finally {
+		await mongoose.connection.close();
+	}
 };
 seedDB();
--- data/seed-test.js
+++ data/seed-test.js
@@ -1,7 +1,6 @@
 const polygonAPI = require("./seed_test_data/polygon-test");
-const polygonAPI2 = require("./seed_test_data/polygon2-test");
-const ESGdata = require("./seed_test_data/esg-test");
-const Userdata = require("./seed_test_data/userdata-test");
+const esgData = require("./seed_test_data/esg-test");
+const userData = require("./seed_test_data/userdata-test");
 
 const ESG = require("../schema/esgSchema");
 const Polygon = require("../schema/polygonSchema");
@@ -9,32 +8,31 @@ const Users = require("../schema/usersSchema");
 
 const mongoose = require("mongoose");
 
-const uri = "INSER MONGO URI HERE";
+const uri = "INSERT URI";
 
 const seedDB = async () => {
-  try {
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+	try {
+		await mongoose.connect(uri, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
 
-    await Polygon.deleteMany({});
-    await ESG.deleteMany({});
-    await Users.deleteMany({});
+		await Polygon.deleteMany({});
+		await ESG.deleteMany({});
+		await Users.deleteMany({});
 
-    await Polygon.insertMany(polygonAPI);
-    await Polygon.insertMany(polygonAPI2);
-    console.log(`stock entries added`);
+		await Polygon.insertMany(polygonAPI);
+		console.log(`stock entries added`);
 
-    await ESG.insertMany(ESGdata);
-    console.log(`esg entries added`);
+		await ESG.insertMany(esgData);
+		console.log(`esg entries added`);
 
-    await Users.insertMany(Userdata);
-    console.log(`user entries added`);
-  } catch (error) {
-    console.log(error);
-  } finally {
-    await mongoose.connection.close();
-  }
+		await Users.insertMany(userData);
+		console.log(`user entries added`);
+	} catch (error) {
+		console.log(error);
+	} finally {
+		await mongoose.connection.close();
+	}
 };
 seedDB();
