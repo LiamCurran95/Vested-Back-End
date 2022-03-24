def4a4b33e6a246750ed72892b5005256ddb4295
Carl Crozier <crozier83@googlemail.com>
2022-03-24 14:48:31 +0000

    modified user schema file and updated to accept multiple form answers and portfolios

--- schema/usersSchema.js
+++ schema/usersSchema.js
@@ -8,25 +8,30 @@ const formAnswers = new Schema({
   governanceRating: { type: Number },
 });
 
-const userPortfolios = new Schema({ any: Object });
+const userPortfolios = new Schema({
+  any: Object,
+  createdAt: { type: Date, default: Date.now },
+});
 
 const usersSchema = new Schema(
   {
     username: { type: String, required: true },
     email: { type: String, required: true },
     avatarUrl: { type: String, required: false },
-    form_answers: formAnswers,
-    portfolios: userPortfolios,
+    form_answers1: formAnswers,
+    form_answers2: formAnswers,
+    form_answers3: formAnswers,
+    portfolios1: userPortfolios,
+    portfolios2: userPortfolios,
+    portfolios3: userPortfolios,
     newUser: { type: Boolean, required: true, default: true },
     theme: { type: String, required: true, default: "light" },
   },
   {
     versionKey: false,
-  },
+  }
 );
 
 const Users = mongoose.model("User", usersSchema);
 
 module.exports = Users;
-
-// createdAt: { type: Date, default: Date.now },
