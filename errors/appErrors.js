6d2dfda99356f9b7cf3fce793c81c458d83f8857
LiamCurran <LiamCurran95>
2022-03-24 12:40:02 +0000

    Commiting changes

--- errors/appErrors.js
+++
@@ -1,7 +0,0 @@
-exports.invalidUrlError = (res) => {
-  res.status(404).send({ message: "Invalid URL Passed" });
-};
-
-exports.methodError = (res) => {
-  res.status(405).send({ message: "Method not allowed on this route" });
-};
