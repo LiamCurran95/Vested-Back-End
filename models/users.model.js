7bdf677d8462ab9afd66347e41b608c30359e903
Carl Crozier <crozier83@googlemail.com>
2022-03-25 15:15:23 +0000

    tweaks to env variable

--- models/users.model.js
+++ models/users.model.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
-const { testUri } = require("../secretInfo");
 
-const uri = process.env.MONGODB_URI || testUri;
+const uri = process.env.MONGODB_URI;
 const User = require("../schema/usersSchema");
 
 exports.fetchUsers = async () => {
