ad62f61a139e034402b39edf45a8511bb2748340
LiamCurran <LiamCurran95>
2022-03-28 11:11:25 +0100

    Refactoring tests and adding additional error handling

--- app.js
+++ app.js
@@ -1,48 +1,45 @@
-const express = require('express');
-
+const express = require("express");
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
 //ERRORS
-const errors = require('./controllers/errors.controller');
+const errors = require("./controllers/errors.controller");
 
 //POLYGON
-const { getPolygon } = require('./controllers/polygon.controller');
+const { getPolygon } = require("./controllers/polygon.controller");
 
 //ESG
-const { getESG } = require('./controllers/esg.controller');
+const { getESG } = require("./controllers/esg.controller");
 
 //USERS
 
 const {
-  getUsers,
-  getUserByUsername,
-  emptyPortfolio,
-
-  getPortfolioByUsername,
-  postUserAnswers,
-} = require('./controllers/users.controller');
+	getUsers,
+	getUserByUsername,
+	emptyPortfolio,
+	getPortfolioByUsername,
+	postUserAnswers,
+} = require("./controllers/users.controller");
 
 //POLYGON
-app.get('/api/polygon', getPolygon);
+app.get("/api/polygon", getPolygon);
 
 //ESG
-app.get('/api/ESG', getESG);
+app.get("/api/ESG", getESG);
 
 //USERS
-app.get('/api/users', getUsers);
-app.get('/api/users/:username', getUserByUsername);
-app.get('/api/:username/:portfolio', getPortfolioByUsername);
-
-app.patch('/api/:username/:portfolio', emptyPortfolio);
-
-app.patch('/api/users/:username/:formAnswers', postUserAnswers);
+app.get("/api/users", getUsers);
+app.get("/api/users/:username", getUserByUsername);
+app.get("/api/:username/:portfolio", getPortfolioByUsername);
+app.patch("/api/:username/:portfolio", emptyPortfolio);
+app.patch("/api/users/:username/:formAnswers", postUserAnswers);
 
 app.listen(process.env.port || 9090, () => {
-  console.log('Server online..');
+	console.log("Server online..");
 });
 
 app.use(errors.mongooseErrors);
+app.all("/api/*", errors.handler404);
 
 module.exports = app;
--- test/test.js
+++ test/test.js
@@ -8,114 +8,144 @@ chai.should();
 
 chai.use(chaiHttp);
 
-describe('Testing the Vested Back-End', () => {
-  beforeEach(async () => {
-    await seedDB();
-  });
-  describe('/GET/Polygon', () => {
-    it('Fetches all polygon data', (done) => {
-      chai
-        .request(app)
-        .get('/api/polygon')
-        .end((err, res) => {
-          const { result } = res.body;
-          res.should.have.status(200);
-          result.should.have.lengthOf(374);
-          done();
-        });
-    });
-  });
-  describe('/GET/Users', () => {
-    it('Fetches all User data', (done) => {
-      chai
-        .request(app)
-        .get('/api/users')
-        .end((err, res) => {
-          const { result } = res.body;
-          res.should.have.status(200);
-          result.should.have.lengthOf(2);
-          done();
-        });
-    });
-  });
-  describe('/GET/ESG', () => {
-    it('Fetches all ESG data', (done) => {
-      chai
-        .request(app)
-        .get('/api/ESG')
-        .end((err, res) => {
-          const { result } = res.body;
-          res.should.have.status(200);
-          result.should.have.lengthOf(28);
-          done();
-        });
-    });
-  });
-  describe('/api/:username/:portfolio', () => {
-    it("Fetches the users's portfolios", (done) => {
-      chai
-        .request(app)
-        .get('/api/jessjelly/portfolio1')
-        .end((err, res) => {
-          const { result } = res.body;
-          res.should.have.status(200);
-          result.tickers.should.eql(['COST', 'ABT', 'ANET', 'FR', 'A']);
-          done();
-        });
-    });
-  });
-
-  describe('/api/:username/:portfolio', () => {
-    it('sets the specfied portfolio array to empty', (done) => {
-      chai
-        .request(app)
-        .patch('/api/jessjelly/portfolio1')
-        .end((err, res) => {
-          const { result } = res.body;
-          res.should.have.status(200);
-          result.tickers.should.eql([]);
-        done();
-        });
-    });
-  });
-
-  describe("PATCH /API/Users/:username/:formAnswers", () => {
-    it("Status 201 - Overwrites a users form responses on their user profile", (done) => {
-      const username = "jessjelly";
-      const formAnswers = "formAnswers1";
-      const formResponses = {
-        environmentalRating: 1,
-        socialRating: 1,
-        governanceRating: 3,
-      };
-      chai.request(app)
-        .patch(`/api/users/${username}/${formAnswers}`)
-        .send({ formResponses })
-        .end((err, res) => {
-          const { result } = res.body;
-          res.should.have.status(201);
-          result.formAnswers1.environmentalRating.should.equal(1);
-          result.formAnswers1.socialRating.should.equal(1);
-          result.formAnswers1.governanceRating.should.equal(3);
-          done();
-        });
-    });
-    it("Status 404 - Incorrect username format", (done) => {
-      const username = "jessjelly123";
-      const formAnswers = "formAnswers1";
-      const formResponses = {
-        environmentalRating: 1,
-        socialRating: 1,
-        governanceRating: 3,
-      };
-      chai.request(app)
-        .patch(`/api/users/${username}/${formAnswers}`)
-        .send({ formResponses })
-        .end((err, res) => {
-          res.should.have.status(404);
-          res.body.should.eql({ msg: "Bad request." });
-          done();
-        });
-    });
-  });
+describe("Testing the Vested Back-End", () => {
+	beforeEach(async () => {
+		await seedDB();
+	});
+	describe("GET /api/Polygon", () => {
+		it("Status 200 - Return contains all polygon data", (done) => {
+			chai
+				.request(app)
+				.get("/api/polygon")
+				.end((err, res) => {
+					const { result } = res.body;
+					res.should.have.status(200);
+					result.should.have.lengthOf(374);
+					done();
+				});
+		});
+		it("Status 404 - Invalid path", (done) => {
+			chai
+				.request(app)
+				.get("/api/polygons123")
+				.end((err, res) => {
+					res.status.should.eql(404);
+					res.body.should.eql({ msg: "Invalid path" });
+					done();
+				});
+		});
+	});
+	describe("GET /api/users", () => {
+		it("Status 200 - Return contains all User data", (done) => {
+			chai
+				.request(app)
+				.get("/api/users")
+				.end((err, res) => {
+					const { result } = res.body;
+					res.should.have.status(200);
+					result.should.have.lengthOf(2);
+					done();
+				});
+		});
+		it("Status 404 - Invalid path", (done) => {
+			chai
+				.request(app)
+				.get("/api/userssers")
+				.end((err, res) => {
+					res.status.should.eql(404);
+					res.body.should.eql({ msg: "Invalid path" });
+					done();
+				});
+		});
+	});
+	describe("GET /api/ESG", () => {
+		it("Status 200 - Return contains all ESG data", (done) => {
+			chai
+				.request(app)
+				.get("/api/ESG")
+				.end((err, res) => {
+					const { result } = res.body;
+					res.should.have.status(200);
+					result.should.have.lengthOf(28);
+					done();
+				});
+		});
+		it("Status 404 - Invalid path", (done) => {
+			chai
+				.request(app)
+				.get("/api/esggesseg")
+				.end((err, res) => {
+					res.status.should.eql(404);
+					res.body.should.eql({ msg: "Invalid path" });
+					done();
+				});
+		});
+	});
+	describe("GET api/:username/:portfolio", () => {
+		it("Status 200 - Return contains specific user portfolio", (done) => {
+			chai
+				.request(app)
+				.get("/api/jessjelly/portfolio1")
+				.end((err, res) => {
+					const { result } = res.body;
+					res.should.have.status(200);
+					result.tickers.should.eql(["COST", "ABT", "ANET", "FR", "A"]);
+					done();
+				});
+		});
+	});
+	describe("PATCH /api/:username/:portfolio", () => {
+		it("Status 200 - Method updates specific portfolio array to empty", (done) => {
+			chai
+				.request(app)
+				.patch("/api/jessjelly/portfolio1")
+				.end((err, res) => {
+					const { result } = res.body;
+					res.should.have.status(200);
+					result.tickers.should.eql([]);
+					done();
+				});
+		});
+	});
+	describe("PATCH /API/Users/:username/:formAnswers", () => {
+		it("Status 201 - Overwrites a users form responses on their user profile", (done) => {
+			const username = "jessjelly";
+			const formAnswers = "formAnswers1";
+			const formResponses = {
+				environmentalRating: 1,
+				socialRating: 1,
+				governanceRating: 3,
+			};
+			chai
+				.request(app)
+				.patch(`/api/users/${username}/${formAnswers}`)
+				.send({ formResponses })
+				.end((err, res) => {
+					const { result } = res.body;
+					res.should.have.status(201);
+					result.formAnswers1.environmentalRating.should.equal(1);
+					result.formAnswers1.socialRating.should.equal(1);
+					result.formAnswers1.governanceRating.should.equal(3);
+					done();
+				});
+		});
+		it("Status 404 - Incorrect username format", (done) => {
+			const username = "jessjelly123";
+			const formAnswers = "formAnswers1";
+			const formResponses = {
+				environmentalRating: 1,
+				socialRating: 1,
+				governanceRating: 3,
+			};
+			chai
+				.request(app)
+				.patch(`/api/users/${username}/${formAnswers}`)
+				.send({ formResponses })
+				.end((err, res) => {
+					res.should.have.status(404);
+					res.body.should.eql({ msg: "Bad request." });
+					done();
+				});
+		});
+	});
 });
