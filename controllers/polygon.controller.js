519d6a30dce4c55efc6de01e959140bff226ae2a
LiamCurran <LiamCurran95>
2022-03-30 11:11:54 +0100

    hotfix polygon

--- controllers/polygon.controller.js
+++ controllers/polygon.controller.js
@@ -3,7 +3,7 @@ const { fetchPolygon } = require("../models/polygon.model");
 exports.getPolygon = (req, res, next) => {
 	fetchPolygon()
 		.then((result) => {
-			res.status(200).send({ result: result });
+			res.status(200).send({ result });
 		})
 		.catch((err) => {
 			next(err);
